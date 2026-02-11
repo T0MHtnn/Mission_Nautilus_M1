4c1efc5b73c7b1c34b8dbb3942002c5847370c34
aminemaslak <mohammed-amine.maslak@etu.univ-lyon1.fr>
2026-02-11 21:16:51 +0100

    Finalisation du déploiement TP3 et CI/CD

--- admin/webpack.config.js
+++ admin/webpack.config.js
@@ -1,33 +1,36 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
+const webpack = require('webpack'); // AJOUTÉ pour gérer les variables globales
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-
 const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
-
-
 
 const config = {
     entry: './src/index.ts',
     output: {
         path: path.resolve(__dirname, 'dist'),
+        // Important pour que les assets soient cherchés au bon endroit sur la VM
+        publicPath: isProduction ? '/admin/' : '/', 
     },
     devServer: {
         open: true,
         host: 'localhost',
-        port: 3306
+        port: 3306, // Port demandé dans le sujet
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: 'index.html',
         }),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+        
+        // Point 5 : Injection de l'URL de l'API selon l'environnement
+        new webpack.DefinePlugin({
+            API_PATH: isProduction 
+                ? JSON.stringify('https://192.168.75.88/api') // URL de ta VM
+                : JSON.stringify('http://localhost:3000')      // URL locale
+        }),
     ],
     module: {
         rules: [
@@ -44,14 +47,10 @@ const config = {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: 'asset',
             },
-
             {
                 test: /\.html$/i,
                 use: ['html-loader'],
             },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
         ],
     },
     resolve: {
@@ -62,10 +61,7 @@ const config = {
 module.exports = () => {
     if (isProduction) {
         config.mode = 'production';
-
         config.plugins.push(new MiniCssExtractPlugin());
-
-
     } else {
         config.mode = 'development';
     }
